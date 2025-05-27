8dc3fba8e114c71d6268e44e6baafdd5ea2f533b
ANAND KUMAR <90023958+ANAND9KUMAR@users.noreply.github.com>
2025-05-27 16:34:14 +0530

    Create the sign in and sign up pages

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className='text-white-950'>HomePage</div>
+    <div className='text-blue-950'>HomePage</div>
   )
 }
